75e52c1f6aec00600d84379c80fc1adfd8eabc79
Karol <kstachowicz@icloud.com>
2022-05-04 13:33:43 +0200

    Make this app into a snake game

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 "use strict";
 var container = document.querySelector('#container');
-
+function getRandomInt () {
+    const min = Math.ceil(0);
+    const max = Math.floor(256);
+    return Math.floor(Math.random() * (max - min) + min);
+}
 function paintGrid(gridSize) {
     for (var i = 0; i < gridSize; i++) {
         var colCurrent = document.createElement('div');
@@ -22,20 +26,43 @@ let gridNodes = document.querySelectorAll('.grid');
 function onHover (event) {
     event.addEventListener('mouseover', function () {
             console.log(event);
-            event.classList.add('red');
+            if (event.classList.contains('red')) {
+                sleepTime = 200;
+            }
+            else {
+                event.classList.add('red');
+            }
+            if (event.id === "green") {
+                sleepTime += 100;
+                event.id = "";
+                addFood();
+            }
         });
 }
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
-
+let sleepTime = 300;
 async function onMouseOut (event) {
     event.addEventListener('mouseout', async() => {
-        await sleep(300);
+        await sleep(sleepTime);
         event.classList.remove('red');
     });
 }
+
+async function addFood () {
+    await sleep(1000);
+    let apple = gridNodes[getRandomInt()]
+    apple.id = "green";
+    await sleep(4000);
+    if (apple.id === "green") {
+        apple.id = "";
+        addFood();
+    }
+
+}
+addFood();
 gridNodes.forEach(onHover);
 gridNodes.forEach(onMouseOut);
 
